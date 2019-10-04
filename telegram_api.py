5cbd095cbeb8f7af4ca198cf6a7c7d59a001fd3d
MrKinauJr <46278197+MrKinauJr@users.noreply.github.com>
2019-10-04 20:29:28 +0200

    Update telegram_api.py
    now you dont need to input the token everytime. 
    
    telegram = telegram("token")
    telegram.telegram_text("test","1337420")

--- telegram_api.py
+++ telegram_api.py
@@ -1,23 +1,27 @@
 from requests import post
 from urllib.parse import quote as qt
 
-def telegram_text(text, token, chat_id):
-    post('https://api.telegram.org/bot' + token + '/sendMessage?chat_id=' + chat_id + '&text=' + qt(text))
+class telegram(object):
+    def __init__(self,token):
+        self.token = token
+    
+    def telegram_text(self,text, chat_id):
+        post('https://api.telegram.org/bot' + self.token + '/sendMessage?chat_id=' + chat_id + '&text=' + qt(text))
 
-def telegram_pic(file, token, chat_id, caption = None):
-    if caption:
-        post('https://api.telegram.org/bot' + token + '/sendPhoto?chat_id=' + chat_id + '&caption=' + qt(caption), files = {'photo': file})
-    else:
-        post('https://api.telegram.org/bot' + token + '/sendPhoto?chat_id=' + chat_id, files = {'photo': file})
+    def telegram_pic(self,file, chat_id, caption = None):
+        if caption:
+            post('https://api.telegram.org/bot' + self.token + '/sendPhoto?chat_id=' + chat_id + '&caption=' + qt(caption), files = {'photo': file})
+        else:
+            post('https://api.telegram.org/bot' + self.token + '/sendPhoto?chat_id=' + chat_id, files = {'photo': file})
 
-def telegram_video(file, token, chat_id, caption = None):
-    if caption:
-        post('https://api.telegram.org/bot' + token + '/sendVideo?chat_id=' + chat_id + '&caption=' + qt(caption), files = {'video': file})
-    else:
-        post('https://api.telegram.org/bot' + token + '/sendVideo?chat_id=' + chat_id, files = {'video': file})
+    def telegram_video(self,file, chat_id, caption = None):
+        if caption:
+            post('https://api.telegram.org/bot' + self.token + '/sendVideo?chat_id=' + chat_id + '&caption=' + qt(caption), files = {'video': file})
+        else:
+            post('https://api.telegram.org/bot' + token + '/sendVideo?chat_id=' + chat_id, files = {'video': file})
 
-def telegram_doc(file, token, chat_id, caption = None):
-    if caption:
-        post('https://api.telegram.org/bot' + token + '/sendDocument?chat_id=' + chat_id + '&caption=' + qt(caption), files = {'document': file})
-    else:
-        post('https://api.telegram.org/bot' + token + '/sendDocument?chat_id=' + chat_id, files = {'document': file})
+    def telegram_doc(self,file, chat_id, caption = None):
+        if caption:
+            post('https://api.telegram.org/bot' + self.token + '/sendDocument?chat_id=' + chat_id + '&caption=' + qt(caption), files = {'document': file})
+        else:
+            post('https://api.telegram.org/bot' + self.token + '/sendDocument?chat_id=' + chat_id, files = {'document': file})
